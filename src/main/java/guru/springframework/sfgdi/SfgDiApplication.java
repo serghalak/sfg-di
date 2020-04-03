e966a354eb5f027eb0f14f7516ee2d3b6117c94b
serghalak <serghalak@gmail.com>
2020-04-03 15:34:44 +0300

    using Qualifiers

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,6 +17,21 @@ public class SfgDiApplication {
 		MyController myController = context.getBean("myController", MyController.class);
 		String greeting=myController.sayHello();
 		System.out.println(greeting);
+
+		System.out.println("------------Property");
+		PropertyInjectedController propertyInjectedController=context.getBean(
+				"propertyInjectedController",PropertyInjectedController.class);
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------------Set");
+		SetterInjectedController setterInjectedController=
+				context.getBean("setterInjectedController",SetterInjectedController.class);
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------------Constructor");
+		ConstructorInjectedController constructorInjectedController=
+				context.getBean("constructorInjectedController",ConstructorInjectedController.class);
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,7 +10,8 @@ public class SetterInjectedController {
 
     private GreetingService greetingService;
 
-    public void setGreetingService(GreetingService greetingService) {
+    @Autowired
+    public void setGreetingService(@Qualifier("setterInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
