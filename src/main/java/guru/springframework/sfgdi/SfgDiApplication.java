142da2c0b28ad6c4041fd2e26acb1e357aee8185
serghalak <serghalak@gmail.com>
2020-04-05 13:10:11 +0300

    using @Profile

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -15,6 +12,10 @@ public class SfgDiApplication {
 
 		ConfigurableApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController = context.getBean("myController", MyController.class);
+
+		I18nController i18nController=context.getBean("i18nController",I18nController.class);
+		System.out.println(i18nController.sayGreeting());
+
 		String greeting=myController.sayHello();
 		System.out.println("------------Primary Bean");
 		System.out.println(greeting);
